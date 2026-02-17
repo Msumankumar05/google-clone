2aa02f0a2f03c0c008e6dc0235769ef66fbf8bad
M suman kumar <suman10112004@gmail.com>
2026-02-17 11:58:53 +0530

    Delete google-clone directory

--- google-clone/script.js
+++
@@ -1,8 +0,0 @@
-function validateSearch() {
-    const value = document.getElementById("searchInput").value.trim();
-    if (!value) {
-        alert("Please enter something to search");
-        return false;
-    }
-    return true;
-}
